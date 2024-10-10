54e6aaaea78f86731d6baff6eeb856546380bda6
= <=>
2024-10-09 22:22:34 -0600

    empezando el endpoint de la api del login

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Http\Request;
+
 abstract class Controller
 {
-    //
+    public function login(Request $request){
+        $email = $request->input('email');
+        $password = $request->input('password');
+        $user = User::where('email', $email)->where('password','=', $password)->first();
+        return response()->json([
+            "user"=>$email
+            //"password"=>$token
+        ], 200);
+    }
 }
