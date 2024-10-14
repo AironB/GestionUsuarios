204bb640705955d54ba348871f3609ae17582106
= <=>
2024-10-13 22:49:16 -0600

    agregando controles de tokens

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Foundation\Auth\User;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Schedule;
 use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
@@ -12,15 +17,61 @@ class UserController extends Controller
         $email = $request->input('email');
         $password = $request->input('password');
         $user = User::where('email', $email)->where('password','=', $password)->first();
-        return response()->json([
-            "user"=>$email
-            //"password"=>$token
-        ], 200);
+        $expiresAt = Carbon::now()->addSeconds(60);
+        if($user && Hash::check($password, $user->password)){
+            $token=$user->createToken('api-token',['*'])->expiresAt($expiresAt)->plainTextToken;
+            return response()->json([
+                "user"=>$email,
+                "token"=>$token,
+                "expires_at"=>$expiresAt->toDateTimeString()
+                //"password"=>$token
+            ], 200);
+        }
+        return response()->json(["message"=>"You are not Authorized"], 401);
+       
     }
+    // public function refreshToken(){
+    //     $user = Auth::user();
+    //     if(!$user->tokenCan('api-token')){
+    //     return response()->json(['message'=>'Token invalido o expirado'], 401);
+    // }
+    // $user->token('api-token')->expiresAt(Carbon::now()->addSeconds(60));
+    // return response()->json(['message'=>'Token renovado']);
+    // }
     public function users_by_year(Request $request){
         $year = $request->input('year');
-        $user = User::where('year', $year);
+        if(!$year){
+                $year = request()->year;
+        }
+        $users = User::select([
+        'id', 'name', 'email', 'created_at'
+        ])->whereYear('created_at', $year)->get();
+        if(count($users)>0){
+        return response()->json($users, 200);
+        }
+        return response()->json([], 400);
        
+    }
+    public function users_by_month(Request $request){
+        $month = $request->input('month');
+        if(!$month){
+            $month = request()->month;
+        }
+        $users = User::select([
+            'id','name','email','created_at'
+        ])->whereMonth('created_at', $month)->get();
+        if(count($users)>0){
+            return response()->json($users, 200);
+        }
+        return response()->json([],400);
+    }
+    public function users_by_week(){
+        $usuariosPorSemana=DB::table('users')->select(
+            DB::raw('YEAR(created_at) as anio'),
+            DB::raw('WEEK(created_at) as semana'),
+            DB::raw('COUNT(*) as total_usuarios'))
+            ->groupBy('anio', 'semana')->get();
+        return response()->json($usuariosPorSemana);
     }
     public function add_user(Request $request){
         $validator = Validator::make($request->all(),[
@@ -43,5 +94,35 @@ class UserController extends Controller
         $user->save();
 
         return response()->json(['message'=>'Successfully registered'], 201);
+    }
+    public function update_email(Request $request, $id){
+        $validator = Validator::make($request->all(),[
+            'email'=>'required|string'
+        ]);
+        if($validator->fails()){
+            return response()->json([
+                'message'=>'Validation Error',
+                'errors'=>$validator->errors()
+            ]);
+        }
+        $user = User::find($id);
+        $user->email=$request->input('email');
+        $user->update();
+        return response()->json(['message'=>'Email succesfully updated']);
+    }
+    public function update_password(Request $request, $id){
+        $validator = Validator::make($request->all(),[
+            'password'=>'required|string'
+        ]);
+        if($validator->fails()){
+            return response()->json([
+                'message'=>'Validation Error',
+                'errors'=>$validator->errors()
+            ]);
+        }
+        $user = User::find($id);
+        $user->password = $request->input('password');
+        $user->update();
+        return response()->json(['message'=>'password succesfully updated']);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -7,5 +7,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-Route::post('/V1/login', [UserController::class, 'login']);
+Route::middleware('auth:sanctum')->group(function(){
+    Route::post('/V1/login', [UserController::class, 'login']);
+});
+
 Route::post('/V1/add_user', [UserController::class, 'add_user']);
+Route::get('/V1/users_by_year', [UserController::class, 'users_by_year']);
+Route::get('/V1/users_by_month', [UserController::class, 'users_by_month']);
+Route::get('/V1/users_by_week', [UserController::class, 'users_by_week']);
+Route::patch('/V1/update_email/{id}', [UserController::class, 'update_email']);
+Route::patch('/V1/update_password/{id}', [UserController::class, 'update_password']);
