b18da5c0221ac48a53b72c6683eec784272bec9b
= <=>
2024-10-14 20:24:59 -0600

    subiendo el xml de insomnia

--- routes/api.php
+++ routes/api.php
@@ -9,11 +9,12 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 Route::middleware('auth:sanctum')->group(function(){
     Route::post('/V1/login', [UserController::class, 'login']);
+    Route::post('/V1/add_user', [UserController::class, 'add_user']);
+    Route::patch('/V1/update_email/{id}', [UserController::class, 'update_email']);
+Route::patch('/V1/update_password/{id}', [UserController::class, 'update_password']);
 });
 
-Route::post('/V1/add_user', [UserController::class, 'add_user']);
+
 Route::get('/V1/users_by_year', [UserController::class, 'users_by_year']);
 Route::get('/V1/users_by_month', [UserController::class, 'users_by_month']);
 Route::get('/V1/users_by_week', [UserController::class, 'users_by_week']);
-Route::patch('/V1/update_email/{id}', [UserController::class, 'update_email']);
-Route::patch('/V1/update_password/{id}', [UserController::class, 'update_password']);
